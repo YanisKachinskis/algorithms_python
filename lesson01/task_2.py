f8b31d9fedb1bcadb506df53ea006c1e8d42489c
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2020-05-16 10:30:24 +0300

    Поправил блок-схему, task_2 и task_4 с учетом замечаний.

--- lesson01/task_2.py
+++ lesson01/task_2.py
@@ -1,5 +1,6 @@
 # 6. Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
 
+FIRST_LETTER = 96
 letter_num = int(input('Введите номер буквы: '))
-letter = chr(letter_num + 96)
+letter = chr(letter_num + FIRST_LETTER)
 print(letter)
--- lesson01/task_4.py
+++ lesson01/task_4.py
@@ -1,9 +1,10 @@
 # 5. Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
 
+FIRST_LETTER = 96
 a = input('Введите первую букву: ')
 b = input('Введите вторую букву: ')
-first_number = ord(a) - 96
-second_number = ord(b) - 96
+first_number = ord(a) - FIRST_LETTER
+second_number = ord(b) - FIRST_LETTER
 dif_number = abs(second_number - first_number) - 1
 
 print(f'Номер первой буквы: {first_number}')
